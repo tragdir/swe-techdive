3f1db9cfc2578d1d347c88f4ae0ceb0735cdefda
Wilyendri <wilyendri@gmail.com>
2022-02-09 14:38:49 -0500

    change url

--- server/index.js
+++ server/index.js
@@ -16,7 +16,7 @@ app.use(express.urlencoded({ extended: true })) // for parsing application/x-www
 // we use .env when we use online database url
 // const dbUrl = process.env.DB_URL;
 //const dbUrl = process.env.DB_URL || "mongodb://127.0.0.1:27017/patientdb";
-const uri = "mongodb+srv://hackavengers:hackavengersdb@cluster0.vyrz3.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const uri = "mongodb+srv://malik'Username:<pssw>db@cluster0.vyrz3.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 mongoose
   .connect(uri, {
     useNewUrlParser: true,
