324d4e9e1678fec38a46d99e6f31c07df28cbb22
allysonochoa <83464382+allysonochoa@users.noreply.github.com>
2022-02-21 23:47:47 -0500

    Create footer.js

---
+++ client/src/components/footer.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { AppBar, Toolbar, Typography, Grid } from "@material-ui/core";
+import { Group, Info} from "@material-ui/icons";
+
+const Footer = () => <>
+      <AppBar position="static" color = "primary" >
+       <Grid container justify="center" style={{minHeight: "180px"}}>
+            <Grid container item sm={6} xs={11} justify="space-between">
+                <Grid item sm={5} xs={12}>
+                    <Info color="action" />
+                    <Typography paragraph>
+                        SWE COVID-19 Mini Project 
+                        Hack.Diversity 2022 Cohort.
+                    </Typography>
+                </Grid>
+                <Grid item sm={5} xs={11}>
+                    <Group color="action" />
+                    <Typography paragraph>
+                         Team Hack-Avengers Members:
+                         Amanda Bazelais, Wilyendri Duran, Melaku Tachbele, Malik Siguenza, Nicolas Salazar, 
+                        Vladimir Pierre-Louis & Allyson Ochoa
+                    </Typography>
+                </Grid>
+            </Grid>
+        </Grid>
+      </AppBar>
+    </>
+
+export default Footer;
