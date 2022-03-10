06bf7fe9e33005221a41be002f160633f2418465
Melaku Tachbele <56900055+meltach@users.noreply.github.com>
2022-03-09 23:02:47 -0500

    add vlad footer

--- client/src/pages/NotFound.js
+++ client/src/pages/NotFound.js
@@ -1,42 +1,51 @@
-import { Button, Box, Grid } from "@mui/material";
-import React from "react";
-
-
-
-
+import { Button, Box, Grid } from '@mui/material';
+import React from 'react';
 
 const NotFound = () => {
-
   return (
-
     <div>
-
       <Grid
         container
         justify="center"
         alignItems="center"
         direction="column"
         style={{
-          minHeight: "100vh",
+          minHeight: '100vh',
         }}
         spacing={5}
-      // sx={{ bgcolor: 'success.main' }}
+        // sx={{ bgcolor: 'success.main' }}
       >
-        <Box sx={{ fontStyle: 'italic', fontFamily: 'Rob', color: 'black',  lineHeight: '', fontSize: 100, letterSpacing: 20 }}>404 error</Box>
-        <Box sx={{ color: 'black', lineHeight: "normal", fontSize: 30, letterSpacing: 2 }}> <h4>THE PAGE YOU WERE LOOKING FOR DOES NOT EXIT </h4></Box>
-        <Box sx={{ color: 'black', lineHeight: "normal", fontSize: 30, letterSpacing: 2 }}> <h5>Click The Go Back Button To Return To Previous Page</h5> </Box>
-        <div><Button size="large" variant="contained" color="primary"
-          onClick={() => <div>{window.history.back()}</div>}>Go back</Button></div>
-
-      </Grid></div >
+        <Box
+          sx={{
+            fontStyle: 'italic',
+            fontFamily: 'Rob',
+            color: 'black',
+            lineHeight: '',
+            fontSize: 100,
+            letterSpacing: 20,
+          }}>
+          404 error
+        </Box>
+        <Box sx={{ color: 'black', lineHeight: 'normal', fontSize: 30, letterSpacing: 2 }}>
+          {' '}
+          <h4>THE PAGE YOU WERE LOOKING FOR DOES NOT EXIT </h4>
+        </Box>
+        <Box sx={{ color: 'black', lineHeight: 'normal', fontSize: 30, letterSpacing: 2 }}>
+          {' '}
+          <h5>Click The Go Back Button To Return To Previous Page</h5>{' '}
+        </Box>
+        <div>
+          <Button
+            size="large"
+            variant="contained"
+            color="primary"
+            onClick={() => <div>{window.history.back()}</div>}>
+            Go back
+          </Button>
+        </div>
+      </Grid>
+    </div>
   );
-
 };
-
-
-
-
-
-
 
 export default NotFound;
